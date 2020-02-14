605315dc09d1f571f7f3a3a0438d70833928a8c3
AlexTMallen <atmallen8@gmail.com>
2020-02-14 15:34:50 -0800

    Made initial framework for simulation

--- Car.py
+++ Car.py
@@ -9,3 +9,4 @@ class Car:
 
     def draw(self, surface):
         pygame.draw.fill(surface, self.color, self.rect)
+
--- simulate.py
+++ simulate.py
@@ -1,9 +1,24 @@
 import pygame
+import json
 
 def main():
-    w = pygame.display.set_mode((500, 500))
-    w.fill()
+    # setup
+    with open("colors.json") as f:
+        text = f.read()
+        colors = json.loads(text)
 
+    w = pygame.display.set_mode((500, 500))
+    w.fill(colors["green"])
+
+    waitTime = 10
+    running = True
+
+    while running:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                return
+        pygame.display.flip()
+        pygame.time.wait(waitTime)
 
 if __name__ == "__main__":
     main()
