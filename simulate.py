86274fb195cbd36201bc797b2ff7b6c9e38fb85d
Justin <justinchenyay@gmail.com>
2020-06-09 11:56:53 -0700

    ESC to access options menu, ENTER to go back

--- simulate.py
+++ simulate.py
@@ -6,10 +6,12 @@ from Lane import Lane
 from Car import Car
 from Street import Street
 from Intersection import Intersection
+from Options import Options
 import constants
 
 
 def main():
+    pygame.init()
     np.random.seed(1)
     # setup
     with open("colors.json") as f:
@@ -33,65 +35,92 @@ def main():
     waitTime = 10
     time = 0
     running = True
+    simulating = True
     intersection.changeToCycle(intersection.hgreenCycle)
+
+    options = Options(streetH.numPos, streetH.numNeg, streetH.numLeftOnly, streetV.numPos, streetV.numNeg, streetV.numLeftOnly)
+
+
+
 
     carRects = []
     while running:
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 return
+            if e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_r:
+                    cars = []
+                    time = 0
+                if e.key == pygame.K_ESCAPE:
+                    simulating = False
+                if e.key == pygame.K_RETURN:
+                    simulating = True
 
 
-        if time > intersection.totalCycleLength:
-            time = 0
-        intersection.changeCycle(time)
-        time += 1
-        
 
-        if np.random.randint(20) == 0:
-            street = np.random.choice((streetH, streetV))
-            carLane = street.lanes[np.random.randint(0, len(street.lanes))]
-            car = Car((255, 0, 0), carLane, intersection, desiredSpeed=np.random.normal(1.35, 0.1))
-            if not car.hitBox.collidelistall(carRects):  # Making sure the cars don't overlap when spawned
-                cars.append(car)
+        if simulating:
+            if time > intersection.totalCycleLength:
+                time = 0
+            intersection.changeCycle(time)
+            time += 1
 
-        w.fill(colors["green"])
-        for l in streetH.lanes:
-            l.draw(w)
-        for l in streetV.lanes:
-            l.draw(w)
 
-        intersection.draw(w)
-        carRects = []
-        for car in cars:
-            carRects.append(car.rect)
+            if np.random.randint(20) == 0:
+                street = np.random.choice((streetH, streetV))
+                carLane = street.lanes[np.random.randint(0, len(street.lanes))]
+                car = Car((255, 0, 0), carLane, intersection, desiredSpeed=np.random.normal(1.35, 0.1))
+                if not car.hitBox.collidelistall(carRects):  # Making sure the cars don't overlap when spawned
+                    cars.append(car)
 
-        for i, car in enumerate(cars):
-            if not car.turningLeft:
-                collisionIdxs = car.hitBox.collidelistall(carRects)
-                if collisionIdxs != [i]:
-                    collisionIdxs.remove(i)
-                    if len(collisionIdxs) > 1:
-                        # TODO: better way to handle this?
-                        # print("multiple collisions detected")
-                        cheese = 0
-                    else:
-                        if car.speed != 0:
-                            car.speed = cars[collisionIdxs[0]].speed * 0.7
+            w.fill(colors["green"])
+            for l in streetH.lanes:
+                l.draw(w)
+            for l in streetV.lanes:
+                l.draw(w)
 
-        for c in cars:
-            c.move(carRects)
-            c.draw(w, showHitbox=False)
+            intersection.draw(w)
+            carRects = []
+            for car in cars:
+                carRects.append(car.rect)
 
-            if c.distance >= c.lane.length + 10 * c.LENGTH:
-                cars.remove(c)
+            for i, car in enumerate(cars):
+                if not car.turningLeft:
+                    collisionIdxs = car.hitBox.collidelistall(carRects)
+                    if collisionIdxs != [i]:
+                        collisionIdxs.remove(i)
+                        if len(collisionIdxs) > 1:
+                            # TODO: better way to handle this?
+                            # print("multiple collisions detected")
+                            cheese = 0
+                        else:
+                            if car.speed != 0:
+                                car.speed = cars[collisionIdxs[0]].speed * 0.7
 
-        for light in intersection.lights:
-            light[0].draw(w)
+            for c in cars:
+                c.move(carRects)
+                c.draw(w, showHitbox=False)
 
-        pygame.display.flip()
-        pygame.time.wait(waitTime)
+                if c.distance >= c.lane.length + 10 * c.LENGTH:
+                    cars.remove(c)
+
+            for light in intersection.lights:
+                light[0].draw(w)
+
+
+            pygame.display.flip()
+            pygame.time.wait(waitTime)
+
+        else:
+            options.draw(w)
+
+            pygame.display.flip()
+            pygame.time.wait(waitTime)
+
+
 
 
 if __name__ == "__main__":
     main()
+
+
