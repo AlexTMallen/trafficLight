1abbc29860aa64129da14496c644ac10ce4d25e3
Justin <justinchenyay@gmail.com>
2020-06-09 22:53:56 -0700

    changed options UI

--- Options.py
+++ Options.py
@@ -22,12 +22,12 @@ class Options:
         self.left_column_y = 200
         self.right_column_x = 450
         self.right_column_y = 200
-        self.spacing = 80
+        self.spacing = 40
         for i in range(6):
-            self.inputBoxes.append(pygame.Rect(self.left_column_x + 185, self.left_column_y - 10 + 80 * i, 40, 50))
+            self.inputBoxes.append(pygame.Rect(self.left_column_x + 190, self.left_column_y - 10 + self.spacing * i, 35, 40))
 
         for i in range(4):
-            self.inputBoxes.append(pygame.Rect(self.right_column_x + 192, self.right_column_y - 15 + 80 * i, 40, 50))
+            self.inputBoxes.append(pygame.Rect(self.right_column_x + 195, self.right_column_y - 10 + self.spacing * i, 40, 40))
 
 
         self.typing = False
@@ -46,14 +46,14 @@ class Options:
         Options.text(surface, "Lane Counts", self.left_column_x - 10, 120, 34)
         Options.text(surface, "Simulation Settings", self.right_column_x - 20, 120, 34)
 
-        Options.text(surface, "R to Apply Changes", 80, 700, 26)
+        Options.text(surface, "R to Apply Changes", 500, 500, 26)
 
-        Options.text(surface, "Right: ", self.left_column_x, self.left_column_y, 20)
-        Options.text(surface, "Left:  ", self.left_column_x, self.left_column_y + self.spacing, 20)
-        Options.text(surface, "Horizontal Turn: ", self.left_column_x, self.left_column_y + self.spacing * 2, 20)
-        Options.text(surface, "Down:  ", self.left_column_x, self.left_column_y + self.spacing * 3, 20)
-        Options.text(surface, "Up:    ", self.left_column_x, self.left_column_y + self.spacing * 4, 20)
-        Options.text(surface, "Vertical Turn: ", self.left_column_x, self.left_column_y + self.spacing * 5, 20)
+        Options.text(surface, "# Rightward Lanes: ", self.left_column_x, self.left_column_y, 20)
+        Options.text(surface, "# Leftward Lanes:  ", self.left_column_x, self.left_column_y + self.spacing, 20)
+        Options.text(surface, "# Horz. Left Turn: ", self.left_column_x, self.left_column_y + self.spacing * 2, 20)
+        Options.text(surface, "# Downward Lanes:  ", self.left_column_x, self.left_column_y + self.spacing * 3, 20)
+        Options.text(surface, "# Upward Lanes:    ", self.left_column_x, self.left_column_y + self.spacing * 4, 20)
+        Options.text(surface, "# Vert. Left Turn: ", self.left_column_x, self.left_column_y + self.spacing * 5, 20)
 
         Options.text(surface, "Car Density:", self.right_column_x, self.right_column_y, 20)
         Options.text(surface, "Simulation Speed:", self.right_column_x, self.right_column_y + self.spacing, 20)
@@ -63,10 +63,10 @@ class Options:
 
 
         for i in range(6):
-            Options.text(surface, str(self.index[i]), self.left_column_x + 200, self.left_column_y + 80 * i, 20)
+            Options.text(surface, str(self.index[i]), self.left_column_x + 200, self.left_column_y + self.spacing * i, 20)
 
         for i in range(6, len(self.index)):
-            Options.text(surface, str(self.index[i]), self.right_column_x + 200, self.right_column_y + 80 * (i-6), 20)
+            Options.text(surface, str(self.index[i]), self.right_column_x + 200, self.right_column_y + self.spacing * (i-6), 20)
 
     @staticmethod
     def text(surface, message, x, y, textsize):
